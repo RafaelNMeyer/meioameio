89c438d0a2c9a50ebc4c6fb3b1bf50f26a6d076a
RafaelNMeyer <rafaelniederauer07@gmail.com>
2024-12-23 23:33:16 -0300

    set db client config ssl to true if NODE_ENV is production

--- infra/database.js
+++ infra/database.js
@@ -16,25 +16,31 @@ async function query(queryObject) {
 
 async function getNewClient() {
 
+
+  const { Client } = pg;
+  const client = new Client(getClientConfig());
+
+  await client.connect();
+
+  return client;
+}
+
+function getClientConfig() {
   const user = process.env.POSTGRES_USER;
   const password = process.env.POSTGRES_PASSWORD;
   const host = process.env.POSTGRES_HOST;
   const port = process.env.POSTGRES_PORT;
   const database = process.env.POSTGRES_DB;
 
-  const { Client } = pg;
-  const client = new Client({
+  return {
     user,
     password,
     host,
     port,
     database,
-    ssl: true
-  });
+    ssl: process.env.NODE_ENV == 'production' ? true : false
+  }
 
-  await client.connect();
-
-  return client;
 }
 
 export default { query, getNewClient };
